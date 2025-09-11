81ca15fe72efb1caa549d44f315a007f4bb51fa7
adilya-g <uitaci099@gmail.com>
2025-09-11 18:28:03 +0300

    finished 01

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,7 +1,8 @@
 const reverseString = (str) => {
-    // Начало
-
-    // Конец
+    if(str.length <= 1){
+        return str;
+    }
+    return reverseString(str.slice(str.length/2)) + reverseString(str.slice(0, str.length/2));
 };
 
 export default reverseString;
