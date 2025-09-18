8207d07beca091833d0bdf5b80baf8d5e80e9a01
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-18 16:47:18 +0300

    last cw done

--- Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 6. GenerateDifference/index.js
+++ Lessons/02. Functions. Arrays. Objects. This/Tasks/Task 6. GenerateDifference/index.js
@@ -5,12 +5,31 @@ const getCopy = (el) => JSON.parse(JSON.stringify(el));
 const onlyUnique = (array) => [ ...new Set(array) ];
 
 const generateDifference = (objOne, objTwo) => {
-    let firstObjKeys = objOne.keys;
-    let secondObjKeys = objTwo.keys;
+
+    let firstObjKeys = Object.keys(objOne);
+    let secondObjKeys = Object.keys(objTwo);
+
+
+    let uniqueKeys = [...firstObjKeys, ...secondObjKeys];
+    uniqueKeys = onlyUnique(uniqueKeys);
+
     let result = {};
-    for (let key in firstObjKeys) {
-        if (!(key in secondObjKeys)) {}
+
+    for (let key of uniqueKeys) {
+        if (objOne.hasOwnProperty(key) && !objTwo.hasOwnProperty(key)) {
+            result[key] = 'deleted';
+
+        } else if (objTwo.hasOwnProperty(key) && !objOne.hasOwnProperty(key)) {
+            result[key] = 'added';
+        } else {
+            if (objOne[key] === objTwo[key]) {
+                result[key] = 'unchanged';
+            } else {
+                result[key] = 'changed';
+            }
+        }
     }
+    return result;
 };
 
 export default generateDifference;
