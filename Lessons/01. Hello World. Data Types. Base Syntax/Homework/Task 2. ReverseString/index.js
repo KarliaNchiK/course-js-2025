eb736de30ede8cb974c4306f445e6ceed6bbbd1a
amra238 <amirgimadeev36@gmail.com>
2025-09-19 08:55:33 +0300

    фыф

--- Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
+++ Lessons/01. Hello World. Data Types. Base Syntax/Homework/Task 2. ReverseString/index.js
@@ -1,13 +1,17 @@
 let result = '';
+let temp = '';
 
 const reverseString = (str) => {
-    if (result.length === str.length)
-        return result;
+    if (result.length === str.length) {
+        temp = result;
+        result = '';
+        return temp;
+    }
 
     let pointer = str.length - result.length - 1;
     result += str[pointer];
     return reverseString(str);
 };
 
-let a = reverseString('');
+//let a = reverseString('ab');
 export default reverseString;
