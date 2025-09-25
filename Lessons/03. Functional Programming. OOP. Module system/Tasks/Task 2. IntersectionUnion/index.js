1025b63bac66a7e95d165a7a79d5cc24ebbba374
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-09-25 16:23:25 +0300

    03.c.2 done

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 2. IntersectionUnion/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 2. IntersectionUnion/index.js
@@ -1,5 +1,13 @@
 const getIntersectionUnion = (arrOne, arrTwo) => {
-
+    let intersection = []
+    let union = [...new Set([...arrOne, ...arrTwo])];
+    for (let item of union) {
+        if (arrOne.includes(item) && arrTwo.includes(item))
+        {
+            intersection.push(item);
+        }
+    }
+    return { intersection, union };
 };
 
 export default getIntersectionUnion;
