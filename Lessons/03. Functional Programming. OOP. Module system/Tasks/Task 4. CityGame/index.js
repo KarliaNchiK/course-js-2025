79f5c313a5b5d7a54b35121453a62df8fa699b06
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-10-02 17:34:32 +0300

    lesson 4 + hw

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 4. CityGame/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 4. CityGame/index.js
@@ -1,20 +1,31 @@
 class CityGame {
     namedCities = [];
-    isEven = true;
-
+    isFirstPlayerMove = true;
 
     constructor(firstName, secondName) {
         this.firstName = firstName;
         this.secondName = secondName;
     }
     restart() {
+        this.isFirstPlayerMove = true;
+        this.namedCities = [];
+        return 'The game has been restarted!';
 
     }
-    play(text){
+    play(text) {
         if (this.namedCities.length === 0) {
-            this.isEven = false;
+            this.isFirstPlayerMove = false;
             this.namedCities.push(text);
             return this.namedCities;
+        }
+        if (this.namedCities.at(-1).at(-1).toLowerCase() === text[0].toLowerCase()){
+            this.isFirstPlayerMove = !this.isFirstPlayerMove;
+            this.namedCities.push(text);
+            return this.namedCities;
+        } else {
+            return (this.isFirstPlayerMove === false) ?
+                `Game over! The winner is ${this.firstName}` :
+                `Game over! The winner is ${this.secondName}`;
         }
     }
 
