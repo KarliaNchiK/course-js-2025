f831949dabad5e3a9e9f2914db1a3ea5f3b4521c
Kamil Sabirov <sabirovafarida.kamil@gmail.com>
2025-10-02 17:35:55 +0300

    lesson 5

--- Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 4. CityGame/index.js
+++ Lessons/03. Functional Programming. OOP. Module system/Tasks/Task 4. CityGame/index.js
@@ -12,41 +12,41 @@ class CityGame {
             return `Game over! The winner is ${this.winner}`;
         }
         const cityLower = city.toLowerCase();
+        if (this.usedCities.some(usedCity => usedCity.toLowerCase() === cityLower)) {
+            this.isGameOver = true;
+            if (this.currentPlayerIndex % 2 === 0) {
+                this.winner = this.players[1];
+            } else {
+                this.winner = this.players[0];
+            }
+            return `Game over! The winner is ${this.winner}`;
+        }
+        if (!city || city.trim() === '') {
+            this.isGameOver = true;
+            if (this.currentPlayerIndex % 2 === 0) {
+                this.winner = this.players[1];
+            } else {
+                this.winner = this.players[0];
+            }
+            return `Game over! The winner is ${this.winner}`;
+        }
         if (this.usedCities.length > 0) {
             const lastCity = this.usedCities[this.usedCities.length - 1].toLowerCase();
             const lastLetter = this.getLastLetter(lastCity);
-
             if (cityLower[0] !== lastLetter) {
                 this.isGameOver = true;
-                this.winner = this.players[this.currentPlayerIndex];
+                if (this.currentPlayerIndex % 2 === 0) {
+                    this.winner = this.players[1];
+                } else {
+                    this.winner = this.players[0];
+                }
                 return `Game over! The winner is ${this.winner}`;
             }
         }
-
-        // 2. Город не должен быть уже использован
-        if (this.usedCities.some(usedCity => usedCity.toLowerCase() === cityLower)) {
-            this.isGameOver = true;
-            this.winner = this.players[this.currentPlayerIndex];
-            return `Game over! The winner is ${this.winner}`;
-        }
-
-        // 3. Город должен быть допустимым (не пустая строка)
-        if (!city || city.trim() === '') {
-            this.isGameOver = true;
-            this.winner = this.players[this.currentPlayerIndex];
-            return `Game over! The winner is ${this.winner}`;
-        }
-
-        // Если все проверки пройдены, добавляем город
         this.usedCities.push(city);
-
-        // Передаем ход следующему игроку
-        this.currentPlayerIndex = (this.currentPlayerIndex + 1) % this.players.length;
-
-        // Возвращаем текущий список городов
+        this.currentPlayerIndex = (this.currentPlayerIndex + 1);
         return [...this.usedCities];
     }
-
     restart() {
         this.usedCities = [];
         this.currentPlayerIndex = 0;
@@ -54,35 +54,13 @@ class CityGame {
         this.winner = null;
         return 'The game has been restarted!';
     }
-
-    // Вспомогательный метод для получения последней буквы
-    // (исключаем мягкий знак и другие небуквенные символы)
     getLastLetter(city) {
-        const letters = city.replace(/[^a-zа-яё]/gi, ''); // Убираем не-буквы
+        const letters = city.replace(/[^a-zа-яё]/gi, '');
         let lastChar = letters[letters.length - 1];
-
-        // Если последняя буква - мягкий знак, берем предыдущую
         if (lastChar === 'ь' || lastChar === 'ъ') {
             lastChar = letters[letters.length - 2];
         }
-
         return lastChar;
-    }
-
-    // Дополнительные методы для информации о состоянии игры
-    getCurrentPlayer() {
-        return this.players[this.currentPlayerIndex];
-    }
-
-    getUsedCities() {
-        return [...this.usedCities];
-    }
-
-    getGameStatus() {
-        if (this.isGameOver) {
-            return `Game over! Winner: ${this.winner}`;
-        }
-        return `Game in progress. Current player: ${this.getCurrentPlayer()}`;
     }
 }
 
