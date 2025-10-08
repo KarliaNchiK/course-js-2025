4c3e284575be2714f04a8af42cb8970e50ceabf7
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-10-09 00:48:28 +0300

    05.c.1 done

--- Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
@@ -1,3 +1,18 @@
-const setCounter = () => {};
+const setCounter = () => {
+
+    let btn = document.createElement('button');
+    let clickAmt = 0;
+
+    btn.textContent = `Количество нажатий: ${clickAmt}`;
+
+    btn.addEventListener('click', () => {
+        clickAmt += 1;
+        btn.textContent = `Количество нажатий: ${clickAmt}`;
+        btn.className = (clickAmt === 3) ? 'btn btn-warning' : '';
+    })
+
+    let parent = document.querySelector('div.main');
+    parent.append(btn);
+};
 
 export default setCounter;
