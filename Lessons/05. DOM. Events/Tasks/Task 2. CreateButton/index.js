8cbffedfe6f6ef89e50fecc6e68e5d5fad46bfb3
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-10-09 01:56:18 +0300

    05.c.2 and 05.c.3 done

--- Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 2. CreateButton/index.js
@@ -1,5 +1,18 @@
 const createButton = () => {
+    let btn = document.createElement("button");
+    btn.className = 'btn btn-primary';
+    btn.textContent = 'Кнопка';
 
+    let btnContainer = document.querySelector('div.button-container');
+    let description = document.querySelector('div.description');
+
+    btnContainer.append(btn);
+    btn.addEventListener('click', () => {
+        let span = document.createElement("span");
+        span.textContent = 'Текст вставлен!';
+
+        description.append(span);
+    })
 };
 
 export default createButton;
--- Lessons/05. DOM. Events/Tasks/Task 3. TrafficLights/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 3. TrafficLights/index.js
@@ -1,3 +1,5 @@
+import {type} from "node:os";
+
 const LIGHTS = {
     red: {
         type: 'red',
@@ -12,17 +14,71 @@ const LIGHTS = {
         text: 'Зеленый',
     },
 };
+let redLight = document.createElement('span');
+let yellowLight = document.createElement('span');
+let greenLight = document.createElement('span');
 
 const resetLights = () => {
-
+    redLight.className = 'red-light';
+    yellowLight.className = '';
+    greenLight.className = '';
 };
 
-const setLight = (type) => {
+const setLight = (value) => {
+    if (value === 'red') {
+        redLight.className = 'red-light';
+        yellowLight.className = '';
+        greenLight.className = '';
+    } else if (value === 'yellow') {
+        redLight.className = '';
+        yellowLight.className = 'yellow-light';
+        greenLight.className = '';
+    } else {
+        redLight.className = '';
+        yellowLight.className = '';
+        greenLight.className = 'green-light';
+    }
 
 };
 
 const setTrafficLight = () => {
+    let traficLights = document.querySelector('.traffic__lights');
+    let traficLightsContainer = document.createElement('div');
+    traficLightsContainer.className = 'traffic__lights-container';
 
+    traficLights.appendChild(traficLightsContainer);
+
+
+
+    redLight.className = 'red-light';
+    traficLightsContainer.appendChild(redLight);
+
+
+    traficLightsContainer.appendChild(yellowLight);
+
+
+    traficLightsContainer.appendChild(greenLight);
+
+
+
+    let traficLightsSelect = document.querySelector('.traffic__lights-select');
+    let select = document.createElement('select');
+
+    let firstOption = document.createElement('option');
+    firstOption.value = 'red';
+    firstOption.textContent = 'Красный';
+    let secondOption = document.createElement('option');
+    secondOption.value = 'yellow';
+    secondOption.textContent = 'Желтый';
+    let thirdOption = document.createElement('option');
+    thirdOption.value = 'green';
+    thirdOption.textContent = 'Зеленый';
+
+    select.addEventListener('change', (option) => {
+        setLight(option.target.value);
+    });
+    traficLightsSelect.appendChild(select);
+    select.append(firstOption, secondOption, thirdOption);
 };
 
 export default setTrafficLight;
