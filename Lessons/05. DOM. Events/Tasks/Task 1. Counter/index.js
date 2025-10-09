967974c161478aa272d86a0755969313ac00af22
wsupni99 <dan.prosv1701@yandex.ru>
2025-10-09 17:17:06 +0300

    Done 05 HW & CW

--- Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
+++ Lessons/05. DOM. Events/Tasks/Task 1. Counter/index.js
@@ -2,12 +2,12 @@ const setCounter = () => {
     let counter = 0;
     const mainDiv = document.querySelector(".main");
     const button = document.createElement("button");
-    const text = "Количество нажатий: "
+    const text = "Количество нажатий:"
     button.classList.add("btn", "btn-warning");
     button.textContent = `${text} ${counter}`;
     button.addEventListener("click", () => {
         counter++;
-        button.textContent = `Количество нажатий: ${counter}`;
+        button.textContent = `${text} ${counter}`;
     });
     mainDiv.appendChild(button);
 };
