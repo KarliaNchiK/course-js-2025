1bdac870390e3fee880eacab99b7d77932ce1ddc
Turbin <dickover@mail.ru>
2025-10-16 00:22:00 +0300

    6_Lesson_1task

--- Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
+++ Lessons/06. Naming. App state. Dom updates. MVC/Tasks/Task 1. ProgressBar/index.js
@@ -1,9 +1,29 @@
-const setWidth = (number) => {
+let currentProgress = 0;
 
+const setWidth = (number) => {
+    const progressBar = document.querySelector('.progress-bar');
+    if (progressBar) {
+        progressBar.style.width = number + '%';
+
+    }
 };
 
 const updateProgress = (defaultNumber, delta) => {
+    currentProgress = defaultNumber;
+    setWidth(currentProgress);
 
+    const increaseButton = document.querySelector('.button-increase');
+    const decreaseButton = document.querySelector('.button-decrease');
+
+    increaseButton.addEventListener('click', () => {
+        currentProgress = Math.min(100, currentProgress + delta);
+        setWidth(currentProgress);
+    });
+
+    decreaseButton.addEventListener('click', () => {
+        currentProgress = Math.max(0, currentProgress - delta);
+        setWidth(currentProgress);
+    });
 };
 
 export default updateProgress;
