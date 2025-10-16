428774860a52eeaa19fad3d49b10107417e5b6c0
Roman.Iskakov <Roman.Iskakov@shire.local>
2025-10-16 11:33:58 +0300

    Занятие 7. Examples

--- Lessons/07. Async Programming. Event Loop. Promise/Examples/errors handling/index.js
+++ Lessons/07. Async Programming. Event Loop. Promise/Examples/errors handling/index.js
@@ -10,26 +10,22 @@ try {
     this.filter = { ...data };
 }
 
-
 // Создание ошибок, их выброс дальше
 const error = new Error('Текст ошибки');
-const error = new SyntaxError('Текст ошибки');
-const error = new ReferenceError('Текст ошибки');
-const error = new TypeError('Текст ошибки');
-
+// const error = new SyntaxError('Текст ошибки');
+// const error = new ReferenceError('Текст ошибки');
+// const error = new TypeError('Текст ошибки');
 
 try {
-    const data = { keys: [ 1, 2, 3 ] };
+    const data = { keys: [1, 2, 3] };
     if (!data.names) {
-        throw new Error("Имена не найдены");
+        throw new Error('Имена не найдены');
     }
     const newData = data.names; // не выполнится
 } catch (error) {
     console.log(error);
     throw error; // "выбрасываем" ошибку для дальнейшего использоваться
 }
-
-
 
 promises.readFile('./files/one', 'utf-8')
     .then((dataOne) => console.log(dataOne))
