1b3934a13e8ce8a38cd4dd2433c7eefe2e8ba7b7
Azaliya <safiullinaazalia79@gmail.com>
2025-11-04 00:10:53 +0300

    commit

--- Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 1. Clocks/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 1. Clocks/index.js
@@ -1,9 +1,32 @@
 const setRotations = () => {
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
 
+    const hourAngle = 30 * hours;
+    const minuteAngle = 6 * minutes;
+    const secondAngle = 6 * seconds;
+
+    const hourHand = document.querySelector('.clocks__hour');
+    const minuteHand = document.querySelector('.clocks__minute');
+    const secondHand = document.querySelector('.clocks__second');
+
+    if (hourHand) {
+        hourHand.style.transform = `rotate(${hourAngle}deg)`;
+    }
+
+    if (minuteHand) {
+        minuteHand.style.transform = `rotate(${minuteAngle}deg)`;
+    }
+
+    if (secondHand) {
+        secondHand.style.transform = `rotate(${secondAngle}deg)`;
+    }
 };
 
 const runClocks = () => {
-
+    setRotations();
+    setInterval(setRotations, 1000);
 };
-
 export default runClocks;
