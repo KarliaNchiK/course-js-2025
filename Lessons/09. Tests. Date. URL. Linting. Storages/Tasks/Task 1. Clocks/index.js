d40c0a2791d20b423cd67517aad868f53d5e6ed2
amra238 <amirgimadeev36@gmail.com>
2025-11-06 17:31:47 +0300

    deef

--- Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 1. Clocks/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 1. Clocks/index.js
@@ -1,9 +1,18 @@
 const setRotations = () => {
+    let hours = new Date().getHours();
+    let minute = new Date().getMinutes();
+    let seconds = new Date().getSeconds();
+    const spans = document.querySelectorAll('span');
 
+    spans[0].style = `transform: rotate(${hours * 15}deg);`
+    spans[1].style = `transform: rotate(${minute * 6}deg);`
+    spans[2].style = `transform: rotate(${seconds * 6}deg);`
+
+    setTimeout(setRotations, 1000);
 };
 
 const runClocks = () => {
-
+    setRotations();
 };
 
 export default runClocks;
