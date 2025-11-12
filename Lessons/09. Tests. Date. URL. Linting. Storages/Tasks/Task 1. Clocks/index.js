58f1274ad158c2a868d4548a8b933e8a701707cf
Turbin <dickover@mail.ru>
2025-11-13 00:39:13 +0300

    09-lesson-classwork

--- Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 1. Clocks/index.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 1. Clocks/index.js
@@ -1,9 +1,37 @@
-const setRotations = () => {
 
+const setRotations = () => {
+    const now = new Date();
+
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const seconds = now.getSeconds();
+
+    const hourAngle = (hours % 12 ) * 30;
+    const minuteAngle = minutes * 6;
+    const secondAngle = seconds * 6;
+
+    const hourHand = document.querySelector('.clocks__hour');
+    const minuteHand = document.querySelector('.clocks__minute');
+    const secondHand = document.querySelector('.clocks__second');
+
+    if (hourHand) {
+        hourHand.style.transform = "rotate(" + hourAngle + "deg)";
+    }
+
+    if (minuteHand) {
+        minuteHand.style.transform = "rotate(" + minuteAngle + "deg)";
+    }
+
+    if (secondHand) {
+        secondHand.style.transform = "rotate(" + secondAngle + "deg)";
+    }
 };
 
 const runClocks = () => {
 
+    setRotations();
+
+    setInterval(setRotations, 1000);
 };
 
 export default runClocks;
--- Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
+++ Lessons/09. Tests. Date. URL. Linting. Storages/Tasks/Task 2. Tests/index.test.js
@@ -1,5 +1,57 @@
 import getIsFirstArrayCooler from './index';
 
 describe('09.c.2 getIsFirstArrayCooler', () => {
+    it('09.c.2.1 должен возвращать true, когда в первом массиве больше больших чисел', () => {
+        const numsOne = [5,8,10];
+        const numsTwo = [3,7,9]
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(true);
+    });
 
+    it('09.c.2.2 должен возвращать false, когда во втором массиве числа больше чем в первом', () => {
+        const numsOne = [1,2,3];
+        const numsTwo = [5,5,5];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(false);
+    })
+
+    it('09.c.2.3 должен возвращать false, если массивы имеют разную длину', () => {
+        const numsOne = [1,2,3];
+        const numsTwo = [1,2,3,4];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(false);
+    })
+
+    it('09.c.2.4 должен возвращать false, когда все элементы равны', () => {
+        const numsOne = [1,2,3];
+        const numsTwo = [1,2,3];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(false);
+    })
+
+    it('09.c.2.5 должен возвращать false, когда массивы имеют одинаковые количество побед', () => {
+        const numsOne = [3,2];
+        const numsTwo = [3,2];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(false);
+    })
+
+    it('09.c.2.6 должен возвращать false, когда в массиве разница равна 0', () => {
+        const numsOne = [5,2,3,10];
+        const numsTwo = [2,5,4,7];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(false);
+    })
+
+    it('09.c.2.7 должен возвращать true, когда разница +1', () => {
+        const numsOne = [5,2,10];
+        const numsTwo = [2,5,7];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(true);
+    })
+
+    it('09.c.2.8 должен возвращать false, когда разница -1', () => {
+        const numsOne = [3,5,7];
+        const numsTwo = [5,3,8];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(false);
+    })
+
+    it('09.c.2.9 должен корректно работать с отрицательными числами и нулями', () => {
+        const numsOne = [-1, 0, 5];
+        const numsTwo = [-2, 1, 3];
+        expect(getIsFirstArrayCooler(numsOne,numsTwo)).toBe(true);
+    })
 });
