f953b890f68c33607629f5eef69fae49397d37bd
Azaliya <safiullinaazalia79@gmail.com>
2025-11-13 01:24:50 +0300

    commit

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
@@ -1,75 +1,95 @@
 <template>
-    <div class="card-container">
-        <div
-            v-for="card in cardStates"
-            :key="card.id"
-            class="card"
-        >
-            <img
-                :src="card.src"
-                class="card-img-top"
-            >
-            <div class="card-body">
-                <h5 class="card-title">
-                    {{ card.head }}
-                </h5>
-                <h4 class="card-title">
-                    {{ card.price }}
-                </h4>
-                <p class="card-text">
-                    {{ card.description }}
-                </p>
-                <div class="card__buttons">
-                    <button
-                        class="btn btn-primary button-plus"
-                        @click="increment(card)"
-                    >
-                        +
-                    </button>
-                    <button
-                        class="btn btn-primary button-minus"
-                        @click="decrement(card)"
-                    >
-                        -
-                    </button>
-                    <div>{{ card.number }} шт.</div>
-                </div>
-                <div
-                    v-if="card.number > card.max"
-                    class="card__alert alert alert-danger"
-                >
-                    Данного товара доступно только {{ card.max }} шт.
-                </div>
-            </div>
+  <div class="card-container">
+    <div
+        v-for="card in cardStates"
+        :key="card.id"
+        class="card"
+    >
+      <img
+          :src="card.src"
+          class="card-img-top"
+      >
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ card.head }}
+        </h5>
+        <h4 class="card-title">
+          {{ card.price }}
+        </h4>
+        <p class="card-text">
+          {{ card.description }}
+        </p>
+        <div class="card__buttons">
+          <button
+              class="btn btn-primary button-plus"
+              @click="increment(card)"
+          >
+            +
+          </button>
+          <button
+              class="btn btn-primary button-minus"
+              @click="decrement(card)"
+          >
+            -
+          </button>
+          <div>{{ card.number }} шт.</div>
         </div>
+        <div
+            v-if="card.number > card.max"
+            class="card__alert alert alert-danger"
+        >
+          Данного товара доступно только {{ card.max }} шт.
+        </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Cards',
-    props: {
-        cards: {
-            type: Array,
-            required: true,
+  name: 'Cards',
+  data() {
+    return {
+      cardStates: [
+        {
+          id: 0,
+          src: '/Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/assets/iphone10.jpg',
+          description: 'Packed with Innovative Features Including a Super Retina Display, TrueDepth Camera System, Face ID and A11 Bionic Chip with Neural Engine',
+          head: 'Iphone X 64GB',
+          price: '800$',
+          number: 0,
+          max: 4,
         },
-    },
-    data() {
-        return {
-            cardStates: this.cards.map(card => ({ ...card })),
-        };
-    },
-    methods: {
-        increment(card) {
-            // eslint-disable-next-line no-plusplus
-            card.number++;
+        {
+          id: 1,
+          src: '/Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/assets/galaxys21.jpg',
+          description: 'Introducing Galaxy S21 Ultra 5G. Designed with a unique contour-cut camera to create a revolution in photography — letting you capture cinematic 8K video and snap epic stills, all in one go.',
+          head: 'Samsung Galaxy S21 128GB',
+          price: '1000$',
+          number: 0,
+          max: 10,
         },
-        decrement(card) {
-            if (card.number > 0) {
-                // eslint-disable-next-line no-plusplus
-                card.number--;
-            }
+        {
+          id: 2,
+          src: '/Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/assets/nokia2600.jpg',
+          description: 'Give me back my 2007',
+          head: 'Nokia 2600',
+          price: 'Priceless',
+          number: 0,
+          max: 1,
         },
+      ],
+    };
+  },
+  methods: {
+    increment(card) {
+      card.number++;
     },
+    decrement(card) {
+      if (card.number > 0) {
+        card.number--;
+      }
+    },
+  },
 };
 </script>
