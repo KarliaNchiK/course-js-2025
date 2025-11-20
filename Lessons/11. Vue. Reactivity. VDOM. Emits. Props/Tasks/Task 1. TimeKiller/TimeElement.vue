d0d40653e13f79f66d012ea31504c91ecd6d1e62
Danil Romanov <danillyou@mail.ru>
2025-11-20 16:16:39 +0300

    dop commit

---
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/TimeElement.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="times-container">
+    <div class="times-container__item"
+         v-for="(block, index) in blocks"
+         :key="index"
+         @mouseover="removeBlock(index)">
+      {{block.timestamp}}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'TimeElement',
+  emits: ["remove-block"],
+  props: {
+    blocks: {
+      type: Array,
+      required: true
+    }
+  },
+  methods: {
+    removeBlock(index) {
+      this.$emit("remove-block", index);
+    }
+  }
+
+}
+
+</script>
+
+<style>
+.times-container__item {
+  padding: 10px;
+  margin-bottom: 10px;
+  background: #fffdb1;
+  border: 1px solid #e1dfa2;
+  border-radius: 4px;
+}
+</style>
