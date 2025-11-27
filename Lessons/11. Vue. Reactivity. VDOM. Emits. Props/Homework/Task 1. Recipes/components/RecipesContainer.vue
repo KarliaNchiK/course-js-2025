f16401e123e7669b14b3096617196904375e021f
SharanskiyRdk <almosahap@gmail.com>
2025-11-27 14:56:20 +0300

    update snap for 10,11,12,13 lessons and add tasks for 11 lesson

--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/components/RecipesContainer.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/components/RecipesContainer.vue
@@ -1,25 +1,83 @@
 <template>
-    <!--Начало-->
-    <div>
-        <h3>
-            Мои рецепты
-        </h3>
-        <div class="card" />
+  <div class="recipe-container">
+    <h3>Мои рецепты</h3>
+
+    <div v-if="recipes.length === 0" class="recipe-container__no-recipes">
+      Рецептов нет
     </div>
-<!--Конец-->
+
+    <div v-else>
+      <div
+          v-for="recipe in recipes"
+          :key="recipe.id"
+          class="card"
+      >
+        <div class="card-body">
+          <div class="card-body__header">
+            <h4>{{ recipe.name }}</h4>
+            <VeganIcon />
+            {{ recipe.isVegan ? 'Да' : 'Нет' }}
+            <TimeIcon />
+            {{ getCookTimeText(recipe.cookTime) }}
+          </div>
+          Ингредиенты: {{ recipe.ingredients }}
+          <button
+              class="btn btn-light card-body__remove-button"
+              @click="removeRecipe(recipe.id)"
+          >
+            Удалить рецепт
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-// Начало
+import VeganIcon from '../icons/VeganIcon.vue'
+import TimeIcon from '../icons/TimeIcon.vue'
+
 export default {
-    name: 'RecipesContainer',
-    components: {
+  name: 'RecipesContainer',
+  components: {
+    VeganIcon,
+    TimeIcon
+  },
+  props: {
+    recipes: {
+      type: Array,
+      required: true
+    }
+  },
+  emits: ['remove-recipe'],
+  methods: {
+    removeRecipe(recipeId) {
+      this.$emit('remove-recipe', recipeId);
     },
-    props: {
-    },
+    getCookTimeText(cookTimeValue) {
+      const cookTimes = {
+        1: '5 минут',
+        2: '10 минут',
+        3: '15 минут',
+        4: '20 минут',
+        5: '30 минут',
+        6: '40 минут',
+        7: '50 минут',
+        8: '1 час',
+        9: '1,5 часа',
+        10: '2 часа',
+        11: '2,5 часа',
+        12: '3 часа',
+        13: 'Бесконечность'
+      };
+      return cookTimes[cookTimeValue] || '5 минут';
+    }
+  }
 };
-// Конец
 </script>
 
 <style>
+.recipe-container {
+  max-width: 350px;
+}
 </style>
--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/App.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Tasks/Task 1. TimeKiller/App.vue
@@ -1,26 +1,52 @@
 <template>
-    <div class="time-killer">
-        <!--Начало-->
-        <!--Конец-->
-    </div>
+  <div class="time-killer">
+    <button
+        type="button"
+        class="btn btn-warning"
+        @click="addTime"
+    >
+      Добавить время!
+    </button>
+    <TimeElement
+        :times="times"
+        @remove-time="removeTime"
+    />
+  </div>
 </template>
 
 <script>
+import TimeElement from './TimeElement.vue'
+
 export default {
-    name: 'TimeKiller',
-    // Начало
-    // Конец
+  name: 'TimeKiller',
+  components: {
+    TimeElement
+  },
+  data() {
+    return {
+      times: []
+    }
+  },
+  methods: {
+    addTime() {
+      const currentTime = new Date().toString();
+      this.times.push(currentTime);
+    },
+    removeTime(index) {
+      this.times.splice(index, 1);
+    }
+  }
 };
 </script>
 
 <style>
 .time-killer {
-    margin: 100px;
-    display: flex;
+  margin: 100px;
+  display: flex;
 }
 
 .time-killer > button {
-    margin-right: 20px;
-    align-self: baseline;
+  margin-right: 20px;
+  align-self: baseline;
 }
 </style>
