61cdf53f9de478ed37914c1690ad525a8c17f841
Turbin <dickover@mail.ru>
2025-11-27 16:25:05 +0300

    10 lesson

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="card-container">
+  <div class="card-container" :cards="cards">
     <div class="card" v-for="card in cards" :key="card.id">
       <img :src="card.src" class="card-img-top" />
 
@@ -24,7 +24,7 @@
           <div>{{card.number}} шт.</div>
         </div>
 
-        <div v-if="card.number > card.max" class="card__alert alert alert-danger"
+        <div v-if="card.number === card.max" class="card__alert alert alert-danger"
         >
           Данного товара доступно только {{ card.max }} шт.
         </div>
@@ -39,52 +39,21 @@ export default {
   name: 'App',
 
   props: {
-    cards: Array,
-    default: () => [
-        {
-          id: 0,
-          src: "/Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/assets/iphone10.jpg",
-          description:
-              "Packed with Innovative Features Including a Super Retina Display, TrueDepth Camera System, Face ID and A11 Bionic Chip with Neural Engine",
-          head: "Iphone X 64GB",
-          price: "800$",
-          number: 0,
-          max: 4,
-        },
-
-            {
-              id: 1,
-              src: "/Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/assets/galaxys21.jpg",
-              description:
-                  "Introducing Galaxy S21 Ultra 5G. Designed with a unique contour-cut camera to create a revolution in photography — letting you capture cinematic 8K video and snap epic stills, all in one go.",
-              head: "Samsung Galaxy S21 128GB",
-              price: "1000$",
-              number: 0,
-              max: 10,
-            },
-
-            {
-              id: 2,
-              src: "/Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/assets/nokia2600.jpg",
-              description: "Give me back my 2007",
-              head: "Nokia 2600",
-              price: "Priceless",
-              number: 0,
-              max: 1,
-            },
-        ]
+    cards: {
+      type: Array,
+      required: true
+    }
   },
 
   data() {
-    return {
-
-
-    };
+    return {};
   },
 
   methods: {
     plus(card) {
-      card.number++;
+      if(card.number < card.max) {
+        card.number++;
+      }
     },
 
     minus(card) {
@@ -93,5 +62,6 @@ export default {
       }
     },
   },
+
 };
 </script>
