c8f47050295ab270b88d2a4d85f0ab1a3417909c
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-11-27 15:47:49 +0300

    10 hw done? exception in 10.h.2

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 1. Cards/App.vue
@@ -1,8 +1,82 @@
 <template>
+  <div class="card-container">
+    <div
+        class="card"
+        v-for="card in cards"
+        :key="card.id"
+    >
+      <img :src="card.src" class="card-img-top" />
+
+      <div class="card-body">
+        <h5 class="card-title">
+          {{ card.head }}
+        </h5>
+
+        <h4 class="card-title">
+          {{ card.price }}
+        </h4>
+
+        <p class="card-text">
+          {{ card.description }}
+        </p>
+
+        <div class="card__buttons">
+          <button
+              class="btn btn-primary button-plus"
+              @click="increment(card)"
+          >
+            +
+          </button>
+          <button
+              class="btn btn-primary button-minus"
+              @click="decrement(card)"
+          >
+            -
+          </button>
+          <div>{{ card.number }} шт.</div>
+        </div>
+
+        <div
+            v-if="card.number > card.max"
+            class="card__alert alert alert-danger"
+        >
+          Данного товара доступно только {{ card.max }} шт.
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'Cards',
+  name: 'Cards',
+
+  // главное отличие: принимаем массив карточек извне
+  props: {
+    cards: {
+      type: Array,
+      required: true,
+    },
+  },
+
+  // тесты ожидают, что data существует и является функцией
+  data() {
+    return {};
+  },
+
+  methods: {
+    increment(card) {
+      card.number++;
+    },
+
+    decrement(card) {
+      if (card.number > 0) {
+        card.number--;
+      }
+    },
+  },
 };
 </script>
+
+<style>
+</style>
--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/App.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/App.vue
@@ -1,8 +1,68 @@
 <template>
+    <div
+        data-v-app=""
+    >
+
+        <!--Начало-->
+        <div class="carousel">
+            <div class="carousel-inner">
+                <div
+                    v-for="(src, index) in images"
+                    :key="index"
+                    class="carousel-item"
+                    :class="{ active: index === currentIndex }"
+                >
+                    <img
+                        :src="src"
+                        class="d-block w-100"
+                    >
+                </div>
+            </div>
+            <button
+                class="carousel-control-prev"
+                @click="prev"
+            >
+                <span class="carousel-control-prev-icon" />
+            </button>
+            <button
+                class="carousel-control-next"
+                @click="next"
+            >
+                <span class="carousel-control-next-icon" />
+            </button>
+        </div>
+    <!--Конец-->
+
+    </div>
 </template>
 
 <script>
 export default {
     name: 'Carousel',
+    props: {
+        images: {
+            type: Array,
+            required: true,
+        },
+    },
+    data() {
+        return {
+            currentIndex: 0,
+        };
+    },
+    methods: {
+        next() {
+            if (!this.images || this.images.length === 0) {
+                return;
+            }
+            this.currentIndex = (this.currentIndex + 1) % this.images.length;
+        },
+        prev() {
+            if (!this.images || this.images.length === 0) {
+                return;
+            }
+            this.currentIndex = (this.currentIndex - 1 + this.images.length) % this.images.length;
+        },
+    },
 };
 </script>
