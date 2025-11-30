40af4749a0a06652b1d49c156fee75d4fe89c71e
Turbin <dickover@mail.ru>
2025-11-30 20:15:14 +0300

    fix merge

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/App.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/App.vue
@@ -1,7 +1,6 @@
 
 <template>
 
-  <!--Начало-->
   <div class="carousel">
     <div class="carousel-inner">
       <div
@@ -31,30 +30,11 @@
       <span class="carousel-control-next-icon"></span>
     </button>
   </div>
-  <!--Конец-->
+
 </template>
 
-  <div class="carousel">
-    <div class="carousel-inner">
-      <div v-for="(image, index) in images"
-           :key="index"
-           class="carousel-item"
-           :class="{ active: index === currentIndex }"
-      >
-        <img :src="image"
-             class="d-block w-100"
 
 
-        >
-      </div>
-    </div>
-    <button @click="prevImage" class="carousel-control-prev">
-      <span class="carousel-control-prev-icon"></span>
-    </button>
-    <button @click="nextImage" class="carousel-control-next">
-      <span class="carousel-control-next-icon"></span>
-    </button>
-  </div>
 
 
 <script>
@@ -94,84 +74,22 @@ export default {
 
 
 <style>
-
-
-  name: 'Carousel',
-  props: {
-    images: {
-      type: Array,
-      required: true,
-      default: () => ['/Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/assets/carousel-1.jpg',
-        '/Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/assets/carousel-2.jpg',
-        '/Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/assets/carousel-3.jpg']
-    }
-  },
-  data() {
-    return {
-      currentIndex: 0
-
-    };
-  },
-  methods: {
-    prevImage() {
-      if (this.currentIndex === 0) {
-        this.currentIndex = this.images.length - 1;
-      } else {
-        this.currentIndex--;
-      }
-    },
-
-    nextImage() {
-      if (this.currentIndex === this.images.length - 1) {
-        this.currentIndex = 0;
-      } else {
-        this.currentIndex++;
-      }
-    }
-  }
-};
-</script>
-
-<style scoped>
 .carousel {
-  position: relative;
+  background: #f6f6f6;
+  display: block;
+  padding: 30px;
+  margin: 40px;
+  font-family: Verdana, Tahoma, sans-serif;
+  border-radius: 10px;
+  align-items: center;
+  width: 1000px;
 }
 
 .carousel-item {
-  display: none;
+  min-height: 325px;
 }
 
 .carousel-item.active {
-  display: block;
-}
-
-.carousel-control-prev,
-.carousel-control-next {
-  position: absolute;
-  top: 50%;
-  transform: translateY(-50%);
-  background: black;
-  color: white;
-  border: none;
-  padding: 10px;
-  font-size: 20px;
-  cursor: pointer;
-}
-
-.carousel-control-prev {
-  left: 10px;
-}
-
-.carousel-control-next {
-  right: 10px;
-}
-
-.d-block {
-  display: block;
-}
-
-.w-100 {
-  width: 100%;
+  display: flex;
 }
 </style>
-
--- Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/App.vue
+++ Lessons/11. Vue. Reactivity. VDOM. Emits. Props/Homework/Task 1. Recipes/App.vue
@@ -1,38 +1,15 @@
 <template>
-    <div class="recipes">
-        <!--Начало-->
-        <div>
-          <NewRecipeForm @add-recipe="addRecipe" />
-        </div>
-        <div>
-          <RecipesContainer
-              :recipes="recipes"
-              @delete-recipe="deleteRecipe"
-          />
-        </div>
-        <!--Конец-->
-    </div>
+  <div class="recipes">
+    <!--Начало-->
+      <span>sadad213</span>
+    <!--Конец-->
+  </div>
 </template>
 
 <script>
-import RecipesContainer from './components/RecipesContainer.vue';
-import NewRecipeForm from './components/NewRecipeForm.vue';
-
 export default {
-    name: 'Recipes',
-    components: { NewRecipeForm, RecipesContainer },
-    // Начало
-    data() {
-      return { recipes: [] };
-    },
-    methods: {
-      addRecipe(recipe) {
-        this.recipes.push(recipe);
-      },
-      deleteRecipe(index) {
-        this.recipes.splice(index, 1);
-      },
-    },
+  name: 'Recipes',
+  // Начало
   // Конец
 };
 </script>
@@ -40,18 +17,15 @@ export default {
 <style>
 .recipes {
   width: 1000px;
-  margin: 40px auto;
+  margin: 40px;
   display: flex;
-  justify-content: space-between;
-  gap: 40px;
-  padding: 30px;
+  justify-content: center;
+  padding: 20px 0;
   background: whitesmoke;
   border-radius: 15px;
-  box-sizing: border-box;
 }
 
 .recipes > div {
-  width: calc(50% - 20px);
-  flex-shrink: 0;
+  width: 50%;
 }
 </style>
