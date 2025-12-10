b2ccbb98ebfad8deb3cb2042e342c321fddd56a1
Vladimir Okonesnikov <okoneshnikov.vova@gmail.com>
2025-12-10 23:55:53 +0300

    10.h done

--- Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/App.vue
+++ Lessons/10. Frameworks 101. Vue.js/Homework/Task 2. Carousel Vue/App.vue
@@ -1,38 +1,30 @@
 <template>
-    <div
-        data-v-app=""
-    >
-
-        <!--Начало-->
-        <div class="carousel">
-            <div class="carousel-inner">
-                <div
-                    v-for="(src, index) in images"
-                    :key="index"
-                    class="carousel-item"
-                    :class="{ active: index === currentIndex }"
+    <div class="carousel">
+        <div class="carousel-inner">
+            <div
+                v-for="(src, index) in images"
+                :key="index"
+                class="carousel-item"
+                :class="{ active: index === currentIndex }"
+            >
+                <img
+                    :src="src"
+                    class="d-block w-100"
                 >
-                    <img
-                        :src="src"
-                        class="d-block w-100"
-                    >
-                </div>
             </div>
-            <button
-                class="carousel-control-prev"
-                @click="prev"
-            >
-                <span class="carousel-control-prev-icon" />
-            </button>
-            <button
-                class="carousel-control-next"
-                @click="next"
-            >
-                <span class="carousel-control-next-icon" />
-            </button>
         </div>
-    <!--Конец-->
-
+        <button
+            class="carousel-control-prev"
+            @click="prev"
+        >
+            <span class="carousel-control-prev-icon" />
+        </button>
+        <button
+            class="carousel-control-next"
+            @click="next"
+        >
+            <span class="carousel-control-next-icon" />
+        </button>
     </div>
 </template>
 
